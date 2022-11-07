8665921735cd55d0d51b97daa1db1f82bb2a24e7
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-11-07 21:38:41 +0700

    feat: add burger menu for main page

--- songbird/src/pages/main/script.js
+++ songbird/src/pages/main/script.js
@@ -4,3 +4,8 @@ import '../../assets/styles/body.css'
 import './main_header.css'
 import './main_main.css'
 import './main_footer.css'
+
+import { burgerOpen, openBurger, closeBurger } from '../../assets/js/burger'
+
+burgerOpen.onclick = openBurger;
+document.onclick = closeBurger;
