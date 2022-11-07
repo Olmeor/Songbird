39672a4d5b8934d5db55c1835f06c905d18c27ad
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-11-07 22:34:42 +0700

    refactor: make common header and footer

--- songbird/src/pages/main/script.js
+++ songbird/src/pages/main/script.js
@@ -1,9 +1,9 @@
 import '../../assets/styles/fonts.css'
 import '../../assets/styles/normalize.css'
 import '../../assets/styles/body.css'
-import './main_header.css'
-import './main_main.css'
-import './main_footer.css'
+import '../../assets/styles/header.css'
+import '../../assets/styles/footer.css'
+import './main.css'
 
 import { burgerOpen, openBurger, closeBurger } from '../../assets/js/burger'
 
